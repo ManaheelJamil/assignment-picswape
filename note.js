0d7e9c79dc4c0bc589c14df0fb19938e050b2e92
Manaheeljamil <manaheeljamil20@gmail.com>
2022-06-05 09:36:56 -0700

    First commit

---
+++ note.js
@@ -0,0 +1,173 @@
+// var rightNow = new Date();
+// var theDay = rightNow.getDay();
+//  var findDay = new Date();
+//  var newDay = findDay.getDay();
+// console.log(newDay)
+// var monthName = ["january", "february", "march", "april", "june"];
+// var month = new Date();
+// var theDay = month.getFullYear();
+// var finalAnswer = monthName[theDay];
+// console.log(finalAnswer)
+//  var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+//  var now = new Date();
+//  var theDay = now.getMonth();
+//  var nameOfToday = dayNames[theDay];
+//  console.log(nameOfTo
+// var theDay = new Date();
+// var rightNow = theDay.getTime();
+// console.log(rightNow)
+//  function tellTime() {
+//  var now = new Date();
+//  var theHr = now.getHours();
+//  var theMin = now.getMinutes();
+//  alert("Current time: "+ theHr + ":" + theMin);
+// }
+// tellTime();
+// function name() {
+//     alert("hi manaheel");
+// }
+// name("hi manaheel");
+// var manaheel = "what is your name";
+// fatherName(manaheel);
+// function fatherName(manaheel) {
+//     alert(manaheel);
+// }
+// manaheel();
+// var userName =prompt("what is your name?");
+// fatherName(userName);
+
+// function fatherName(userName) {
+//     alert("welcome" + " " + userName + " " + "thanks for joining me");
+// }
+// var sum;
+// addNumbers();
+//  function addNumbers() {
+//      var sum = 2 + 2;
+//      alert(sum);
+ 
+//  return alert(sum);
+// var userName = prompt("what is your name?");
+
+//  switch(userName) {
+//      case "manaheel" :
+//          alert("HI Manaheel");
+//          break;
+//     case "noorain" :
+//         alert("HI  Noori Anti");
+//         break;
+//     case "hifza" :
+//         alert("HI Chota Bheem");
+//         break;
+//     default :
+//     alert("you are not allowed!");
+//  }
+// var userName = prompt("what is your age?");
+// switch(userName) {
+//      case "15" :
+//          alert("allowed");
+//          break;
+//     case "20" :
+//         alert("allowed");
+//         break;
+//     case "30" :
+//         alert("allowed");
+//     break;
+//     default :
+//     alert("you are not allowed!");
+// }
+// var userName = "manaheel";
+// for (var i = 0; i <= 3; i++) {
+//  alert(i);
+// }
+
+// var i = 0;
+// while (i <= 3) {
+//     alert(i);
+//     i++;
+// }
+// var i = 0;
+// do {
+// alert(i);
+// i++;
+// } while (i <= 3);
+
+//  <script>
+//  function sayHi() {
+//  alert("Hello world!");
+//  }
+//  function sayBye() {
+//  alert("Buh-bye!");
+//  }
+//  </script>
+
+//  function popup(message) {
+//      alert(message);
+//      }
+//      function popup(message) {
+//          alert(message);
+//         }
+//     function makeFieldYellow() {
+//         alert("hi manaheel");
+//     }
+//     function makeFieldWhite() {
+//         alert("ok!");
+//     }
+// function email() {
+//     if (document.getElementById(email).value === "mm") {
+//     alert("please enter the email");
+//  }
+// }
+//  fnction checkAddress(fieldId) {
+//      if (document.getElementById(fieldId).value === "") {
+//      alert("Email address required.");
+//  }
+// }
+// function answer() {
+//     var userName;
+//     var fatherName = document.getElementById("zip").value;
+//     switch(fatherName) {
+//         case "manaheel" :
+//             userName = "manaheeljamil06@gmail.com";
+//             break;
+//         case "noorain" :
+//             userName = "noorainjamil04@gmail.com";
+//             break;
+//         case "hifza" :
+//             userName = "hifzajamil06@gmail.com";
+//             break;
+//     }
+//  alert(userName);
+// }
+// function fillCity() {
+//      var cityName;
+//      var zipEntered = document.getElementById("zipi").value;
+//      switch (zipEntered) {
+//      case "6" :
+//      cityName = "Chicago";
+//      break;
+//      case "m" :
+//      cityName = "Omaha";
+//      break;
+//      case "5" :
+//      cityName = "M";
+//      break;
+//      }
+//      document.getElementById("city").value = cityName;
+//      }
+// function manaheelLoris() {
+//  var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates whichmake up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety ofdistinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have severaladaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time.Slow lorises have a toxic bite, a rare trait among mammals.";
+//      document.getElementById("noorainLoris").innerHTML = expandedParagraph;
+// }
+// function makeInvisible() {
+//     document.getElementById("ugly").className = "hidden";
+// }
+// function manaheel() {
+//     document.getElementById("mana").className = "hidden";
+// }
+// function jamil() {
+//     document.getElementById("manaheel").src = "n.jpg";
+// }
+// function makeBig() {
+//     document.getElementById("p1").className += " big";
+//     }
+    
